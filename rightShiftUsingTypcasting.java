0717201e6ba4890d8f8bd3cbb41dfcdc3f9e2c77
Om Take <omtake427@gmail.com>
2025-08-16 16:29:18 +0530

    Add files via upload

---
+++ rightShiftUsingTypcasting.java
@@ -0,0 +1,13 @@
+public class DemoPro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int no = 97;
+		int result=no>>3;
+		System.out.println(result);
+		System.out.println((float)no/8);
+		System.out.println(Integer.toBinaryString(result));
+
+	}
+
+}
