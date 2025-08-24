78f28a54d7f44182e813d91f3654bc6c8c36ba16
Om Take <omtake427@gmail.com>
2025-08-24 22:21:02 +0530

    Add files via upload

---
+++ SquarePattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SquarePattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter size of square: ");
+        int n = scanner.nextInt();
+
+   
+        for (int i = 0; i < n; i++) {      
+            for (int j = 0; j < n; j++) {     
+                System.out.print("* ");
+            }
+            System.out.println();  
+        }
+
+        scanner.close();
+    }
+}
